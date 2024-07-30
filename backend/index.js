561446dee7ab4d9c82af3e6310cf2fefede26ecb
sajakshrestha8 <shresthasajak123@gmail.com>
2024-07-30 06:48:57 +0545

    logic has been updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
+const reqData = [];
+const symbol = "/?!@#$%&*";
+const num = "123456789";
+const capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const small = "abcdefghijklmnooqrstuvwxyz";
 
 const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Checking my res");
+});
+
+app.post("/", (req, res) => {
+  const payload = req.body;
+  let Data = symbol + num + capital + small;
+  for (let i = 0; i <= 7; i++) {
+    let value = Math.random(Data);
+    console.log(value);
+  }
+  res.send(payload);
+});
 
 app.listen(port, () => {
   console.log("server runnning in " + port);
